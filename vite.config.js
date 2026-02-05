a4175cb9135fdc5dbd9dad20d047e871780d610c
ujjawal200716 <ujjawalksingh1607@gmail.com>
2026-02-06 02:02:53 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    // This proxy ONLY works on your laptop (npm run dev).
+    // It is IGNORED by Vercel in production.
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
+        target: 'http://localhost:3001' || "https://eduproback.onrender.com",
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
